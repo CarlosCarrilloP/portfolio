ad41c7f37efa7a22146c05759147d7f7d75bbb6c
CarlosCarrilloP <103901668+CarlosCarrilloP@users.noreply.github.com>
2024-07-03 13:49:00 +0200

    ajustes

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,10 @@
-import { defineConfig } from 'astro/config';
-import tailwind from "@astrojs/tailwind";
-import robotsTxt from "astro-robots-txt";
+import { defineConfig } from 'astro/config'
+import tailwind from "@astrojs/tailwind"
 
-import netlify from "@astrojs/netlify";
+import robotsTxt from "astro-robots-txt"
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), robotsTxt()],
-  output: 'static',
-  adapter: netlify()
-});
+  site: 'https://porfolio.dev/'
+})
