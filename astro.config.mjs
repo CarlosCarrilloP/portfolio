b348af059190ac6bbd374ca9f2b79e78610fd58a
CarlosCarrilloP <103901668+CarlosCarrilloP@users.noreply.github.com>
2024-07-03 13:00:36 +0200

    ajustes despliegue

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,13 @@
 import { defineConfig } from 'astro/config'
+import netlify from '@astrojs/netlify';
 import tailwind from "@astrojs/tailwind"
 
 import robotsTxt from "astro-robots-txt"
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), robotsTxt()],
-  site: 'https://porfolio.dev/'
-})
+  output: 'server',
+  adapter: netlify({
+    edgeMiddleware: true
+  }),
+});
