8373eb9c36d1f4a86619f5d9372727681c1eb57d
CarlosCarrilloP <103901668+CarlosCarrilloP@users.noreply.github.com>
2024-07-03 13:04:01 +0200

    as

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,12 @@
-import { defineConfig } from 'astro/config'
-import netlify from '@astrojs/netlify';
-import tailwind from "@astrojs/tailwind"
+import { defineConfig } from 'astro/config';
+import tailwind from "@astrojs/tailwind";
+import robotsTxt from "astro-robots-txt";
 
-import robotsTxt from "astro-robots-txt"
+import netlify from "@astrojs/netlify";
 
 // https://astro.build/config
 export default defineConfig({
-  output: 'server',
-  adapter: netlify({
-    edgeMiddleware: true
-  }),
+  integrations: [tailwind(), robotsTxt()],
+  output: "server",
+  adapter: netlify()
 });
